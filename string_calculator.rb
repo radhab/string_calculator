e7c660caf7eaa3af3bf506efab3be571bc4113f0
radhab <radha.bobbili@icentris.com>
2025-06-20 11:36:49 +0530

    Added code to calculate sum of comma separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
 	def self.add(input) 
 		return 0 if input.to_s.strip.empty?
 		return input.to_i if input =~ /^\d+$/
+		parts = input.split(",")
+		return parts.map(&:to_i).sum if parts.size > 0
   end		
 end

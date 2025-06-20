207ad9e9a0d1ed5a5a75b3d69c6ea3cdadc56dff
radhab <radha.bobbili@icentris.com>
2025-06-20 11:38:59 +0530

    Added test to return sum of any number of comma separated numbers and that is handled with earlier code

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -24,8 +24,12 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1")).to eq(1)
     end
 
-    it 'returns sum of  comma‑separated numbers' do
+    it 'returns sum of two comma‑separated numbers' do
       expect(StringCalculator.add("1,2")).to eq(3)
+    end
+
+    it 'returns sum of any no of  comma‑separated numbers' do
+      expect(StringCalculator.add("1,2,3,4")).to eq(10)
     end
 
   end

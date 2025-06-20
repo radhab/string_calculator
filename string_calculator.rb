699eeae06f301d3d49d24a3604b1de81fad0a51e
radhab <radha.bobbili@icentris.com>
2025-06-20 12:12:00 +0530

    Added code to accept newline as delimiter and fixed regular expression to avoid newline acceptance

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,8 @@
 class StringCalculator
 	def self.add(input) 
 		return 0 if input.to_s.strip.empty?
-		return input.to_i if input =~ /^\d+$/
-		parts = input.split(",")
+		return input.to_i if input =~ /\A\d+\z/
+		parts = input.split(/,|\n/)
 		return parts.map(&:to_i).sum if parts.size > 0
   end		
 end
